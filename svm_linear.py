3e24a61027cc789462a784f9456a6f8c6d06f9ce
ShreyashNandgaonkar <46418251+ShreyashNandgaonkar@users.noreply.github.com>
2019-11-29 23:13:30 +0530

    Add files via upload

---
+++ svm_linear.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv('bill_authentication.csv')
+data.shape
+data.head()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+from sklearn.svm import SVC
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train, y_train)
+y_pred = svclassifier.predict(X_test)
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
